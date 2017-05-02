49517d6e82e33835fbf88537f6e3c6bc440c2de4
yann <yann.boisselier@gmail.com>
2017-05-02 12:34:32 +0200

    modern build

---
+++ app/index.js
@@ -0,0 +1,2 @@
+
+require('./lineChart.js');
---
+++ app/lineChart.js
@@ -0,0 +1,60 @@
+var Chart = require('chart.js')
+var axios = require('axios');
+
+axios.get('api/datas.json').then(function(result) {
+
+  var datas = result.data
+
+  var ctx = document.getElementById("myChart")
+
+  var datasLabels = {
+    abstention_pourcentage:"Pourcentage d'abstention sur le total des inscrits",
+    vote_fn_pourcentage_exprimes:"Pourcentage des votes FN sur le total des votes exprimés**",
+    vote_fn_pourcentage_inscrits:"Pourcentage des votes FN sur le total des inscrits*",
+    blancs_nuls_pourcentage:"Pourcentage des votes blancs et nuls",
+    blancs_nuls_abstention_pourcentage:"Pourcentage des votes blancs, nuls et abstention",
+  }
+
+  var labels = [];
+  var datasets = {
+    abstention_pourcentage:{
+      data:[]
+    },
+    vote_fn_pourcentage_exprimes:{
+      borderColor: "rgba(75,192,192,0.4)",
+      backgroundColor: "rgba(75,192,192,0.4)",
+      data:[]
+    },
+    vote_fn_pourcentage_inscrits:{
+      borderColor: "rgba(50,100,192,1)",
+      backgroundColor: "rgba(50,100,192,0.4)",
+      data:[]
+    }
+  }
+
+  for (var year in datas ) {
+    labels.push(year);
+    for (key in datas[year]) {
+      if (datasets[key] !== undefined) {
+        datasets[key].label = datasLabels[key];
+        datasets[key].data.push(datas[year][key]);
+      }
+    }
+  }
+
+  // construction de l'objet data à passer à chartJS
+  var data = {
+    labels:labels,
+    datasets:[]
+  }
+
+  for (var key in datasets) {
+    data.datasets.push(datasets[key]);
+  }
+
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: data
+  })
+
+})
